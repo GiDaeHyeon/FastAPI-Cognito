17909c21790e6a4701c1dbe09df12beec821c5db
기대현 <dh@gidaehyeon-ui-MacBookPro-2.local>
2022-08-16 09:58:22 +0900

    Add: Login Endpoint

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ async def root() -> JSONResponse:
     return JSONResponse(content={"message": "pong"}, status_code=200)
 
 
+@app.get('/login')
+async def login(request: Request, user_name: str, password: str) -> JSONResponse:
+    tokens = 'tokens', auth.login(user_name=user_name, password=password)
+    return JSONResponse(content=tokens, status_code=200)
+
+
 @app.get("/auth-test", dependencies=[Depends(auth)])
 async def auth_test(request: Request) -> JSONResponse:
     return JSONResponse(content={'message': True}, status_code=200)
